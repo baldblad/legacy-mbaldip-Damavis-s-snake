0a3b391c9d6a29b2032f03b9784fbbd1af991f7a
Martí <marti.baldi01@estudiant.upf.edu>
2022-06-21 12:39:02 +0200

    first draft

---
+++ tests.py
@@ -0,0 +1,37 @@
+# module to run the tests for Damivis's snake problem
+from utils import numberOfAvailableDifferentPaths
+from utils import Board, Snake
+
+# define test inputs:
+test1 = {'board': [4,3],
+         'snake': [[2,2],[3,2],[3,1],[3,0],[2,0],[1,0],[0,0]],
+         'depth': 3,
+         'expect_result': 7}
+
+test2 = {'board': [2,3],
+         'snake': [[0,2],[0,1],[0,0],[1,0],[1,1],[1,2]],
+         'depth': 10,
+         'expect_result': 1}
+
+test3 = {'board': [10,10],
+         'snake': [[5,5],[5,4],[4,4],[4,5]],
+         'depth': 4,
+         'expect_result': 81}
+
+tests = [test1, test2, test3]
+
+# run the tests:
+for test in tests:
+
+    # initialize board and snake
+    board = Board(test['board'])
+    snake = Snake(test['snake'])
+    # todo: assert depth constraints
+
+    result = numberOfAvailableDifferentPaths(board, snake, test['depth'])
+
+    # todo: print inputs
+    if result == test['expect_result']:
+        print('\nTest PASSED with result: ', result)
+    else:
+        print('\nTest FAILED with result: ', result)
---
+++ utils.py
@@ -0,0 +1,15 @@
+# Host for the objects, functions and methods for Damavis's snake problem
+
+# Classes
+class Board:
+    # todo: implement Board class
+    pass
+
+class Snake:
+    # todo: implement Snake class
+    pass
+
+# functions:
+def numberOfAvailableDifferentPaths(board, snake, depth):
+    # todo: implement recursive function to solve the Damavis's snake problem
+    return
